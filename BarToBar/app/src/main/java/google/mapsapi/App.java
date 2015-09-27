83ba24c3ab05f1c02c1bc25f4d6abfd779c2cd93
Harrison Ding <harrisond2015@gmail.com>
2015-09-26 23:14:14 -0500

    Working stuffS

--- BarToBar/app/src/main/java/google/mapsapi/App.java
+++ BarToBar/app/src/main/java/google/mapsapi/App.java
@@ -3,6 +3,7 @@ package google.mapsapi;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseUser;
 
 /**
  * Created by root on 9/26/15.
@@ -12,5 +13,6 @@ public class App extends Application{
         super.onCreate();
         Parse.enableLocalDatastore(this);
         Parse.initialize(this, "yI1bjVW9NGCkUzDoX7NNQy4b84pDj89TemvBVkNp", "lhrWMIQD6BWZLIaWkLXN7kefD44zn0qSnVkqrENQ");
+        ParseUser.enableRevocableSessionInBackground();
     }
 }
--- BarToBar/app/src/main/java/google/mapsapi/UserProfileActivity.java
+++ BarToBar/app/src/main/java/google/mapsapi/UserProfileActivity.java
@@ -1,37 +1,28 @@
 package google.mapsapi;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
-public class UserProfileActivity extends AppCompatActivity {
+import com.parse.ParseUser;
+
+public class UserProfileActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_user_profile, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        TextView userEmail = (TextView)findViewById(R.id.user_profile_email);
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        if(currentUser!=null)
+            userEmail.setText(currentUser.getEmail());
+        else{
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
